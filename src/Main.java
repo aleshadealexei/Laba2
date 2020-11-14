3e898c98323d4ec15dfcaf801b8ab624e64d272f
Алексей <mikhai.alex.99@gmail.com>
2020-11-14 22:16:01 +0300

    InterfaceCommit1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("привет, мой новый пользователь!");
         System.out.println("Hello, world! ! !");
         System.out.println("Feature#1");
         System.out.println("Feature#3");
